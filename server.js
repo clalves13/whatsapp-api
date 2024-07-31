c38eb6a8be5ad4f69ebce9559cfd62e2f00e6190
DerickCarvalho <derickjesiel96@gmail.com>
2024-07-31 17:19:07 -0300

    subindo com certificacao ssl

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const app = require('./src/app');
+const https = require('https');
+const fs = require('fs');
 const { baseWebhookURL } = require('./src/config');
 require('dotenv').config();
+
+const privateKey = fs.readFileSync('./certificados/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('./certificados/cert.pem', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
 
 // Start the server
 const port = process.env.PORT || 3000
@@ -11,7 +17,9 @@ if (!baseWebhookURL) {
   // process.exit(1); // Terminate the application with an error code
 }
 
-app.listen(port, () => {
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
   console.log(`Server running on port ${port}`);
-  console.log('http://sitedotheg.site:4343/api-docs/#/');
+  console.log('https://sitedotheg.site/api-docs/');
 })
